2fc1265017568133848569d515ec0ca370cc37a0
Quentin Delos <oxyloon.quenos@gmail.com>
2024-01-17 13:54:48 +0100

    start project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="FR_fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Surveillance des serveurs</title>
+</head>
+<body>
+    <h1>Page d'accueil</h1>
+    <p>Début du projet : 15/01/2024</p>
+    <br>
+    <p>Par Quentin DELOS & Redwan AZARFANE</p>
+</body>
+</html>
