ff396525dfdca3b72a57158e8457da083407b68f
red1az <160141953+red1az@users.noreply.github.com>
2024-05-07 13:02:36 +0200

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,19 @@
 function opentab(tabname) {
     const sensors = document.querySelector(".sensors");
     const graph = document.querySelector(".graph");
+    const manager = document.querySelector(".manager");
 
     if (tabname === "sensors") {
         sensors.style.zIndex = "1";
         graph.style.zIndex = "0";
+        manager.style.zIndex = "0";
     } else if (tabname === "graph") {
-        graph.style.zIndex = "1";
         sensors.style.zIndex = "0";
+        graph.style.zIndex = "1";
+        manager.style.zIndex = "0";
+    } else if (tabname === "manager") {
+        sensors.style.zIndex = "0";
+        graph.style.zIndex = "0";
+        manager.style.zIndex = "1";
     }
 }
